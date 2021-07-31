ae26270b1fd18deefb451b2f7aecef38bf21a7a0
Toras <efpan@mail.ru>
2021-07-31 11:12:27 +0700

    added unicode_escape

--- lesson01/task01.py
+++ lesson01/task01.py
@@ -3,7 +3,7 @@
 # преобразовать строковые представление в формат Unicode и также проверить тип и содержимое переменных.
 
 word_list = ['разработка', 'сокет', 'декоратор']
-word_list_utf_8 = list(map(lambda word: word.encode(encoding='utf-8'), word_list))
+word_list_utf_8 = list(map(lambda word: word.encode('unicode_escape').decode(), word_list))
 for i in range(len(word_list)):
     print(f'{type(word_list[i])} - {word_list[i]}')
     # new_word = word.encode(encoding='utf-8')
